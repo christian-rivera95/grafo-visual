e8d12604f7ecaccc75d4bba0be13e868ac944c10
Christian Rivera <chris.rivera9582@gmail.com>
2022-03-19 21:45:12 -0600

    Add vertex & add edge functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Graphviz } from "graphviz-react";
+import React from "react";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [graph, setGraph] = React.useState("");
+  const [vertexSet, setVertexSet] = React.useState("");
+  const [edgeSet, setEdgeSet] = React.useState("");
+
+  const AddVertex = () => {
+    const regex = /(?<=\{)(.*?)(?=\})/g;
+    const found = vertexSet.match(regex);
+
+    let object = "";
+
+    if (found) {
+      found.forEach((item) => {
+        object += item + ";";
+      });
+      setGraph(graph + object);
+      console.log("AddVertex");
+    }
+  };
+
+  const paintEdge = () => {
+    unPaintEdge();
+    const regex = /(?<=\()(.*?)(?=\))/g;
+    const found = edgeSet.match(regex);
+    let object = graph;
+
+    if (found) {
+      found.forEach((item) => {
+        const splitVertex = item.split(",");
+        const vertex =
+          splitVertex[0] +
+          " -- " +
+          splitVertex[1] +
+          "[color=red,penwidth=3.0];";
+        object += vertex;
+      });
+      setGraph(object);
+    }
+  };
+
+  const unPaintEdge = () => {
+    const regex = /(?<=\()(.*?)(?=\))/g;
+    const found = edgeSet.match(regex);
+    let object = graph;
+
+    if (found) {
+      found.forEach((item) => {
+        const splitVertex = item.split(",");
+        const vertex =
+          splitVertex[0] +
+          " -- " +
+          splitVertex[1] +
+          "[color=black,penwidth=1.0];";
+        object += vertex;
+      });
+      setGraph(object);
+    }
+  };
+
+  const AddEdge = () => {
+    const regex = /(?<=\()(.*?)(?=\))/g;
+    const found = edgeSet.match(regex);
+
+    let object = "";
+
+    if (found) {
+      found.forEach((item) => {
+        const splitVertex = item.split(",");
+        const vertex = splitVertex[0] + " -- " + splitVertex[1];
+        object += splitVertex[0] + " -- " + splitVertex[1] + ";";
+        setGraph(graph + object);
+      });
+    }
+  };
+
+  const handleVertexChange = (e) => {
+    if (e.target.value.length < 45 && e.target.value !== " ") {
+      setVertexSet(e.target.value);
+    }
+  };
+
+  const handleEdgeChange = (e) => {
+    if (e.target.value.length < 45 && e.target.value !== " ") {
+      setEdgeSet(e.target.value);
+    }
+  };
+
+  React.useEffect(() => {
+    console.log(graph);
+  }, [graph]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={(e) => {
+          handleVertexChange(e);
+        }}
+      />
+      <Button
+        onClick={() => {
+          AddVertex();
+        }}
+      >
+        Add Vertex
+      </Button>
+      <input
+        onChange={(e) => {
+          handleEdgeChange(e);
+        }}
+      />
+      <Button
+        onClick={() => {
+          AddEdge();
+        }}
+      >
+        Add Edge
+      </Button>
+      <Button
+        onClick={() => {
+          paintEdge();
+        }}
+      >
+        Paint Edge
+      </Button>
+      <Graphviz
+        dot={`strict graph {
+          ${graph}
+      }`}
+      />
     </div>
   );
 }
